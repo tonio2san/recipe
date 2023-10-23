d70601b62ea8e6c18b099711684e89b153e95f00
ishuy <small.part3@gmail.com>
2023-10-23 23:19:51 +0900

    ブログの雛形を作成

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import styles from "../styles/Home.module.scss";
+import { client } from "../libs/client";
+import Link from "next/link";
+
+// SSG
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  console.log(data);
+
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+    <div className={styles.container}>
+      {blog.map((blog) => (
+        <li key={blog.id}>
+          <Link href={`blog/${blog.id}`}>
+            {blog.title}
+          </Link>
+        </li>
+      ))}
+    </div>
+  );
+};
